02d9934292fce662a2e6fad56f25cf683365edcd
César Augusto <64142701+cesarvich@users.noreply.github.com>
2022-09-26 18:47:04 -0300

    Add files via upload

---
+++ Mi Primer Discor Bot/Mi Primer Discor Bot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace MyFirstBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "omitido por seguranca da nacao",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("cesar"))
+                    await e.Message.RespondAsync("\nNunca vi mais lindo :sunglasses: ");
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
